512c699a5a925184c53e7546752ff54e18ad42d6
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2020-03-28 23:55:47 +0300

    added tests for UserJdbcService

--- src/main/java/org/levelup/job/list/JobListApplication.java
+++ src/main/java/org/levelup/job/list/JobListApplication.java
@@ -2,6 +2,7 @@ package org.levelup.job.list;
 
 import org.levelup.job.list.domain.Position;
 import org.levelup.job.list.jdbc.PositionJdbcService;
+import org.levelup.job.list.jdbc.UserJdbcService;
 
 import java.sql.SQLException;
 
@@ -27,6 +28,25 @@ public class JobListApplication {
     positionJdbcService.deletePositionById(designer.getId());
     System.out.println("Remaining positions:");
     System.out.println(positionJdbcService.findAllPositions());
-  }
 
+    System.out.println("\nUserJdbcService test");
+    UserJdbcService userJdbcService = new UserJdbcService();
+    System.out.println("User added:");
+    System.out.println(userJdbcService.createUser("Jimmy", "White", "1234 567890"));
+    System.out.println("User added once more:");
+    System.out.println(userJdbcService.createUser("Jimmy", "White", "4321 098765"));
+    System.out.println("Another user added:");
+    System.out.println(userJdbcService.createUser("Michael", "White", "7890 123456"));
+    System.out.println("Whites:");
+    System.out.println(userJdbcService.findByLastName("White"));
+    System.out.println("Jimmy Whites:");
+    System.out.println(userJdbcService.findByNameAndLastName("Jimmy", "White"));
+    System.out.println("Updated 4321 098765 passport owner");
+    System.out.println(userJdbcService.updateUser("4321 098765", "Mark", "Williams"));
+    System.out.println("Removing users");
+    userJdbcService.deleteUserByPassport("1234 567890");
+    userJdbcService.deleteUserByPassport("4321 098765");
+    userJdbcService.deleteUserByPassport("7890 123456");
+    System.out.println("Users were deleted.");
+  }
 }
